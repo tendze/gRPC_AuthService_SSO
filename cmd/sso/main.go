38ec3eff3ba1ca969f8fcd5e8f52f37f6ae46a13
Maxim Dang <dang.maxim2811@gmail.com>
2024-11-12 13:32:20 +0300

    unimplemented server realised

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,7 +1,52 @@
-package sso
+package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+	"sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
-	fmt.Println("hi")
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+
+	// TODO: init app
+	// TODO: start grpc server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+	handler := opts.NewPrettyHandler(os.Stdout)
+	return slog.New(handler)
 }
