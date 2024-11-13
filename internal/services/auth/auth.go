cff1e12cfd29ecf5d50c2666dccdbee06e3b44fe
Maxim Dang <dang.maxim2811@gmail.com>
2024-11-13 23:11:06 +0300

    migrations added

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"sso/internal/domain/models"
+	"sso/internal/lib/jwt"
 	"sso/internal/lib/logger/sl"
 	"sso/internal/storage"
 	"time"
@@ -36,6 +37,11 @@ type UserProvider interface {
 type AppProvider interface {
 	App(ctx context.Context, appID int) (models.App, error)
 }
+
+var (
+	ErrInvalidAppID = errors.New("invalid app id")
+	ErrUserExists   = errors.New("user already exists")
+)
 
 func New(
 	log *slog.Logger,
@@ -87,6 +93,13 @@ func (a *Auth) Login(
 	}
 
 	log.Info("user logged in successfully")
+
+	token, err := jwt.NewToken(user, app, a.tokenTTL)
+	if err != nil {
+		a.log.Error("failed to generate token", sl.Err(err))
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+	return token, nil
 }
 
 func (a *Auth) RegisterNewUser(
@@ -109,6 +122,10 @@ func (a *Auth) RegisterNewUser(
 	}
 	id, err := a.usrSaver.SaveUser(ctx, email, passHash)
 	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			log.Warn("user already exists", sl.Err(err))
+			return 0, fmt.Errorf("%s: %w", op, ErrUserExists)
+		}
 		log.Error("failed to save user", sl.Err(err))
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
@@ -119,5 +136,21 @@ func (a *Auth) IsAdmin(
 	ctx context.Context,
 	userID int64,
 ) (bool, error) {
+	const op = "auth.IsAdmin"
+	log := a.log.With(
+		slog.String("op", op),
+		slog.Int64("user_id", userID),
+	)
 
+	log.Info("checking if user is admin")
+	isAdmin, err := a.usrProvider.IsAdmin(ctx, userID)
+	if err != nil {
+		if errors.Is(err, storage.ErrAppNotFound) {
+			log.Warn("user not found", sl.Err(err))
+			return false, fmt.Errorf("%s: %w", op, ErrInvalidAppID)
+		}
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+	log.Info(fmt.Sprintf("checked if user with id=%d is admin", userID), slog.Bool("is_admin", isAdmin))
+	return isAdmin, nil
 }
